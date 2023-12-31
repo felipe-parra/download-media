34bfb8fb290db27b97d865ce12f7bcc72547ff89
Felipe Parra <fpr29.10@gmail.com>
2023-12-31 17:34:51 -0600

    Added og image

---
+++ src/app/opengraph-image.tsx
@@ -0,0 +1,37 @@
+import { ImageResponse } from "next/og";
+
+export const alt = "DLApp"
+export const size = {
+  width: 1200,
+  height: 630
+}
+
+export const contentType = "image/png"
+
+export const metadata = {
+  title: 'DL App',
+  themeColor: 'dark',
+  viewport: {
+    width: 1,
+  },
+}
+
+export default async function Image() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+          background: "linear-gradient(to bottom right)",
+          fontSize: "5rem",
+          color: "white"
+        }}
+      />
+    ),
+    { ...size }
+  )
+}
