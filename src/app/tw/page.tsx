127844ae9ee59e5c3cc53c9f998b48ebfe09999e
Felipe Parra <fpr29.10@gmail.com>
2023-12-31 17:34:36 -0600

    Added tw page

---
+++ src/app/tw/page.tsx
@@ -0,0 +1,11 @@
+import { PiLightbulbFilamentFill } from "react-icons/pi";
+
+export default function Twitter() {
+  return (
+    <section className="w-full h-dvh flex items-center justify-center">
+      <h3 className='w-full flex flex-col items-center justify-center text-center'>
+        <PiLightbulbFilamentFill className="text-yellow-400 text-3xl animate-pulse" />
+        En construccion</h3>
+    </section>
+  )
+}
