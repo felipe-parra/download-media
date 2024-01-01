ad4092c038b4f37b11ceca3d3dabc4659b0b9953
Felipe Parra <fpr29.10@gmail.com>
2024-01-01 12:50:07 -0600

    Added footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <section className='mx-auto text-center text-sm absolute bottom-2 w-full'>©{new Date().getFullYear()} Developed with ❤️ by Felipe Parra </section>
+  )
+}
