16ea3d2aa5691ad85fa457bddb5c6e5bd54a6132
Felipe Parra <fpr29.10@gmail.com>
2024-04-11 18:31:27 -0600

    Updated text color

--- src/components/drawer.tsx
+++ src/components/drawer.tsx
@@ -28,7 +28,7 @@ export default function Drawer() {
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-6 h-6 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
             </label>
           </div>
-          <div className="flex-1 px-2 mx-2">DLApp</div>
+          <div className="flex-1 px-2 mx-2 dark:text-primary text-primary">DLApp</div>
           <div className="flex-none hidden lg:block">
             <ul className="menu menu-horizontal">
               {/* Navbar menu content here */}
